a810afd251da6d3a5bf88a6ba489b11a5b277cbc
asharma062001 <amit.sharma062001@gmail.com>
2020-01-10 02:49:38 +0530

    Angular Coding Challenge
    Login screen with validation.
    Default username = xyz
                 password: abc
    Home page
    1) A simple login page to redirect employee page or manager page
    
    2) Employee:
    
    a) As Employee I should be able give feedback for each project.
    
    b) I should be able to see list of feedbacks which I gave per project.
    
    c) Feedback list must contain below details and manager review (Manager rating and comments).
    
    d) Form should have EmpName, EmpId, ProjectName, Comments and selfRating(stars-*)
    
    *-bad, **-poor, ***-Average, ****-good, *****-Excellent. e) Add basic validation.
    f) I should be able to edit and delete feedback before manager review. g) If manager reviewed feedback then it should be disabled.

---
+++ README.md
@@ -0,0 +1,24 @@
+Acceptance Criteria:
+
+1) A simple login page to redirect employee page or manager page
+
+2) Employee:
+
+a) As Employee I should be able give feedback for each project.
+
+b) I should be able to see list of feedbacks which I gave per project.
+
+c) Feedback list must contain below details and manager review (Manager rating and comments).
+
+d) Form should have EmpName, EmpId, ProjectName, Comments and selfRating(stars-*)
+
+*-bad, **-poor, ***-Average, ****-good, *****-Excellent. e) Add basic validation.
+f) I should be able to edit and delete feedback before manager review. g) If manager reviewed feedback then it should be disabled.
+
+
+3) Manager:
+
+a) As manager I should be able to see list of feedbacks of each employee.
+
+b) I should be able to give my rating and comments to employee in list itself. c) I should be edit and delete any record.
+Note: Assume and create proper Service. Application should have routing and application must contain at least one Custom Directive, One Custom Filter. 
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,84 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { User } from '@/_models';
+import {AlertService, UserService, AuthenticationService } from '@/_services';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent implements OnInit {
+    currentUser: User;
+    users = [];
+    registerForm: FormGroup;
+    loading = false;
+    submitted = false;
+    constructor(
+        private authenticationService: AuthenticationService,
+        private userService: UserService,
+        private formBuilder: FormBuilder,
+        private router: Router,
+        private alertService: AlertService
+    ) {
+        this.currentUser = this.authenticationService.currentUserValue;
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+        this.registerForm = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            EmpID: ['', Validators.required],
+            Project: ['', Validators.required],
+            rate: ['', Validators.required],
+            Comments: ['', [Validators.required, Validators.minLength(6)]]
+        });
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id)
+            .pipe(first())
+            .subscribe(() => this.loadAllUsers());
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll()
+            .pipe(first())
+            .subscribe(users => this.users = users);
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.userService.register(this.registerForm.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.alertService.success('Information added successful', true);
+                    this.router.navigate(['/login']);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+                //let users = JSON.parse(localStorage.getItem('users')) || [];
+                // console.log("users",users);
+
+            
+               localStorage.setItem('users', JSON.stringify(this.registerForm.value));
+               let users = JSON.parse(localStorage.getItem('users')) || [];
+                console.log("users",users);
+                this.alertService.clear();
+    }
+    
+}
